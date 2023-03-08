505b359f0eb0f9d7f325ec040b05b0de404a3e20
Kholida Magfirah <74235340+kholidamagfirah@users.noreply.github.com>
2023-03-08 15:25:38 +0700

    chapter 03

---
+++ Chapter003/1204003_Kholida Magfirah/run_background_processes.py
@@ -0,0 +1,33 @@
+import multiprocessing
+import time
+
+def foo():
+    name = multiprocessing.current_process().name
+    print ("Memulai %s \n" %name)
+    if name == 'background_process':
+        for i in range(0,5):
+            print('---> %d \n' %i)
+        time.sleep(1)
+    else:
+        for i in range(5,10):
+            print('---> %d \n' %i)
+        time.sleep(1)
+    print ("Akhir %s \n" %name)
+    
+
+if __name__ == '__main__':
+    background_process = multiprocessing.Process\
+                         (name='process latar belakang',\
+                          target=foo)
+    background_process.daemon = True
+
+    NO_background_process = multiprocessing.Process\
+                            (name='tanpa proses latar belakang',\
+                             target=foo)
+    
+    NO_background_process.daemon = False
+    
+    background_process.start()
+    NO_background_process.start()
+    
+
