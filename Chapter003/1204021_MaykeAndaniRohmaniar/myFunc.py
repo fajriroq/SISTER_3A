6924183fd05c6f586f26cd9037598786e1197b80
mayke111 <80704223+mayke111@users.noreply.github.com>
2023-03-08 14:51:31 +0700

    1204021_MaykeAndani

--- Chapter003/1204021_MaykeAndaniRohmaniar/myFunc.py
+++ Chapter003/1204021_MaykeAndaniRohmaniar/myFunc.py
@@ -1,5 +1,5 @@
 def myFunc(i):
-    print ('Table input order: %s' %i)
+    print ('Karyawan mengerjakan tugas: %s' %i)
     for j in range (0,i):
-        print('sending order to kitchen :%s' %j)
+        print('Tugas dikerjakan Karyawan :%s' %j)
     return
