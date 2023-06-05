be6f15c6a68b71d4f671ce7a6173563e23506334
Muhammad Nur Fajri <86406309+fajriroq@users.noreply.github.com>
2023-06-05 22:06:45 +0700

    Delete 1204020_Muhammad_Nur_Fajri directory

--- 1204020_Muhammad_Nur_Fajri/celery/main.py
+++
@@ -1,8 +0,0 @@
-from tasks import download
-
-url = "https://example.com"
-filename = "example.html"
-
-result = download.delay(url, filename)  # Menjalankan tugas secara asinkronus
-print("Menjalankan task!!")
-print(f"Task ID: {result.id}")
--- 1204020_Muhammad_Nur_Fajri/celery/test.py
+++
@@ -1,3 +0,0 @@
-from downloaderApp import download,list
-r = download.delay('https://property145.com/media/documents/agent/property_10/tanah-kavling.png', 'property145.png')
-r.ready()
